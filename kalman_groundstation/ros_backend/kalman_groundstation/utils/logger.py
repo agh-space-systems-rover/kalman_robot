583dc58ea46dad4556331dabb5170abbd238faca
Kacper Iwicki <kacper.iwi@gmail.com>
2023-11-16 15:54:18 +0100

    Rewrote logger

---
+++ kalman_groundstation/ros_backend/kalman_groundstation/utils/logger.py
@@ -0,0 +1,17 @@
+import os
+from datetime import date, datetime
+from ros2pkg.api import get_prefix_path
+
+from geometry_msgs.msg import Vector3
+
+class GpsLogger:
+    def __init__(self, filename_prefix: str) -> None:
+        today = date.today()
+        dir_path = os.path.join(get_prefix_path("kalman_groundstation"), 'log')
+        self.path = os.path.join(dir_path, f'{filename_prefix}_{today}') 
+        self.filename_prefix = filename_prefix
+
+    def log(self, gps: Vector3, description: str = ''):
+        with open(self.path, 'a') as f:
+            line = f'{datetime.now().strftime("%H:%M:%S")} {gps.x:.6f}, {gps.y:.6f}, {gps.z:.6f} {description}\n'
+            f.write(line)
