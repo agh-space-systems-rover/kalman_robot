af94c239a7763d27ac86894c76a9c2a377808c45
Kacper Iwicki <kacper.iwi@gmail.com>
2023-11-25 10:54:45 +0100

    small improvements

--- kalman_drivers/drivers/wheel_driver.py
+++ kalman_drivers/drivers/wheel_driver.py
@@ -4,20 +4,21 @@ import numpy as np
 from std_msgs.msg import UInt8MultiArray
 from struct import pack
 
-from kalman_interfaces.msg import WheelState, WheelStates
+from kalman_interfaces.msg import WheelStates
 
 # TODO change shit below, move to another file
-JETSON_CMD_MOTOR_SET_WHEELS = 0x40
+CMD_MOTOR_SET_WHEELS = 0x40
 METRIC_VELOCITY_TO_MOTOR_VALUE_FACTOR = 1
 
-class wheel_driver(Node):
+class WheelDriver(Node):
     def __init__(self):
         super().__init__('wheel_driver')
         self.create_subscription(WheelStates, '/wheel_controller/state', self.controller_state_received, 1)
-        self.publisher = self.create_publisher(UInt8MultiArray, '/ros2uart', 10)
+        self.publisher = self.create_publisher(UInt8MultiArray, '/master_com/ros_to_master', 10)
 
-    def controller_state_received(self, msg):
-        data = [int(x) for x in [JETSON_CMD_MOTOR_SET_WHEELS, 0x08, 
+
+    def controller_state_received(self, msg: WheelStates):
+        data = [int(x) for x in [CMD_MOTOR_SET_WHEELS, 0x08, 
                                  msg.front_right.velocity * METRIC_VELOCITY_TO_MOTOR_VALUE_FACTOR, 
                                  msg.back_right.velocity * METRIC_VELOCITY_TO_MOTOR_VALUE_FACTOR, 
                                  msg.back_left.velocity * METRIC_VELOCITY_TO_MOTOR_VALUE_FACTOR, 
@@ -32,7 +33,7 @@ class wheel_driver(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    wheelDrv = wheel_driver()
+    wheelDrv = WheelDriver()
     rclpy.spin(wheelDrv)
     rclpy.shutdown()
 
