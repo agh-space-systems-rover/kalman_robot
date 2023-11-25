ee21b7bf14d19eb0c83fb2cb15334f263df8dd69
rayferric <63957587+rayferric@users.noreply.github.com>
2023-11-25 12:39:35 +0100

    docs: remove/add comments

--- kalman_master_driver/master_driver/ueuos_node.py
+++ kalman_master_driver/master_driver/ueuos_node.py
@@ -1,74 +1,7 @@
-# #!/usr/bin/env python3
-
-# import rospy
-# from std_msgs.msg import UInt8, UInt8MultiArray
-# from ueuos.msg import ColorRGB
-# from ueuos.srv import State, StateResponse, Color, ColorResponse
-
-
-# UEUOS_STATE = {0: 'OFF',
-#                1: 'AUTO',
-#                2: 'MANUAL',
-#                3: 'COMPLETED'}
-
-# UEUOS_COLOR = {0: (0, 0, 0),
-#                1: (255, 0, 0),
-#                2: (0, 0, 255),
-#                3: (0, 255, 0)}
-
-# UEUOS_SET_STATE = 0x60
-# UEUOS_SET_STATE_LEN = 1
-
-# UEUOS_SET_COLOR = 0x61
-# UEUOS_SET_COLOR_LEN = 3
-
-
-# class LightsManager:
-#     def __init__(self):
-#         self.state = 2
-#         self.rgb = UEUOS_COLOR[self.state]
-
-#         self.set_state = rospy.Service('/ueuos/set_state', State, self.set_state_callback)
-#         self.state_publisher = rospy.Publisher('/ueuos/state', UInt8, queue_size=10)
-#         self.set_color = rospy.Service('/ueuos/set_color', Color, self.set_color_callback)
-#         self.color_publisher = rospy.Publisher('/ueuos/color', ColorRGB, queue_size=10)
-#         self.uart_publisher = rospy.Publisher('/kalman_rover/ros2uart', UInt8MultiArray, queue_size=10)
-
-#     def publish(self):
-#         self.state_publisher.publish(UInt8(self.state))
-#         self.color_publisher.publish(ColorRGB(*self.rgb))
-
-#     def set_state_callback(self, request):
-#         if request.state in (0, 1, 2, 3):
-#             self.state = request.state
-#             self.rgb = UEUOS_COLOR[self.state]
-#             self.uart_publisher.publish(UInt8MultiArray(data=[UEUOS_SET_STATE, UEUOS_SET_STATE_LEN, self.state]))
-#             return StateResponse(True, f'Stan ustawiony na: {self.state} ({UEUOS_STATE[self.state]})')
-#         else:
-#             return StateResponse(False, f'Podaj stan: {UEUOS_STATE}')
-
-#     def set_color_callback(self, request):
-#         if all(0 <= color <= 255 for color in (request.red, request.green, request.blue)):
-#             self.state = 0
-#             self.rgb = (request.red, request.green, request.blue)
-#             self.uart_publisher.publish(UInt8MultiArray(data=[UEUOS_SET_COLOR, UEUOS_SET_COLOR_LEN, *self.rgb]))
-#             return ColorResponse(True, f'Kolor ustawiony na: {self.rgb}')
-#         else:
-#             return ColorResponse(False, f'Podaj kolor RGB')
-
-
-# if __name__ == '__main__':
-#     lm = LightsManager()
-
 import rclpy
 from rclpy.node import Node
 from kalman_interfaces.srv import SetUeuosColor, SetUeuosEffect, SetUeuosMode
 from std_msgs.msg import UInt8MultiArray
-
-# Example service def:
-# std_msgs/ColorRGBA color
-# ---
-# # no response
 
 CAN_CMD_UEUOS_SET_STATE = 0x60
 CAN_CMD_UEUOS_SET_COLOR = 0x61
