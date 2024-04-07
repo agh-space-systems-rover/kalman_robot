903fde87fe8a6c622242f57fe629ecfae4f9d4fb
Wojciech Wiśniewski <wojciech@funsafe.xyz>
2024-04-07 04:50:22 +0200

    WIP - Improve kalman_groundstation implementation
    Ported old controller wheel drive, currently in separate package `wheel_controller`

--- kalman_groundstation/ros_backend/kalman_groundstation/core/websocket/publisher.py
+++ kalman_groundstation/ros_backend/kalman_groundstation/core/websocket/publisher.py
@@ -2,6 +2,30 @@ import rclpy
 from rclpy.node import Node
 from .message import Message
 
+def force_correct_types(data: dict, target_type: type) -> dict:
+    ## TODO: extend or use a dedicated library
+    type_conversions = {
+        'double': float,
+        'int64': int,
+    }
+    fields_and_field_types = target_type.get_fields_and_field_types()
+
+    ret = {}
+    for key in fields_and_field_types:
+        current_type = fields_and_field_types[key]
+        value = data[key]
+        if value is None:
+            continue
+
+        if current_type in type_conversions:
+            conv = type_conversions[current_type]
+            ret[key] = conv(value)
+        else:
+            ret[key] = value
+
+    return ret
+
+
 
 class Publisher:
     """
@@ -20,6 +44,7 @@ class Publisher:
         self.type = type
 
     def convertToRosMessage(self, data):
+        data = force_correct_types(data, self.type)
         return self.type(**data)
 
     def publish(self, message: Message):
