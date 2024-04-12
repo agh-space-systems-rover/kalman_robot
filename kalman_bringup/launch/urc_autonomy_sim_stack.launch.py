f36eba82df0bdb6b68e3b802423660a1d441c5b9
rayferric <63957587+rayferric@users.noreply.github.com>
2024-04-12 02:28:47 +0200

    chore: remove redundant imports in sim_stack launch

--- kalman_bringup/launch/urc_autonomy_sim_stack.launch.py
+++ kalman_bringup/launch/urc_autonomy_sim_stack.launch.py
@@ -1,8 +1,7 @@
 from ament_index_python import get_package_share_path
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
