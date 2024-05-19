613204db06732db3603e98c5e230015450afc1e4
Kacper Iwicki <kacper.iwi@gmail.com>
2024-05-19 22:39:04 +0200

    changed ros_link config for gs

--- kalman_arm_config/setup.py
+++ kalman_arm_config/setup.py
@@ -1,25 +1,30 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'kalman_arm_config'
+package_name = "kalman_arm_config"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ]
+    + [
+        (os.path.join("lib", "config", os.path.dirname(path)), [path])
+        for path in glob("config" + "/**", recursive=True)
+        if os.path.isfile(path)
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='kacper.iwi@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="todo@todo.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
