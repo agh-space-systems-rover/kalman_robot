1a8098462dc52a29562dc9bde30c29fa45d04f40
Laptop Stacyjny <laptop@stacyjny.pl>
2024-05-26 00:55:23 +0200

    Visualization in rviz2 working

--- kalman_groundstation/ros_backend/launch/groundstation.launch.py
+++ kalman_groundstation/ros_backend/launch/groundstation.launch.py
@@ -1,11 +1,41 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 from ament_index_python import get_package_share_path
 
 def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("kalman_arm_moveit_config"),
+                    "urdf",
+                    "arm.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description, {"ignore_timestamp": True}],
+        remappings=[
+            ('/joint_states', '/arm_controllers/joint_states'),
+        ]
+    )
+
+
     return LaunchDescription(
         [
+            robot_state_publisher,
             Node(
                 package="kalman_groundstation",
                 executable="api_node",
