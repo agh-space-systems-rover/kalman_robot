ae792cf65c0b830d68bfc588a6228e9ef4d5774b
Kacper Iwicki <kacper.iwi@gmail.com>
2024-05-26 15:57:34 -0600

    Arm servo scale as topic

---
+++ kalman_arm_utils/src/arm/servo_param_setter.py
@@ -0,0 +1,57 @@
+import rclpy
+from rclpy import Node
+from std_msgs.msg import Float64
+from rcl_interfaces.msg import Parameter, ParameterType, ParameterValue
+from rcl_interfaces.srv import SetParameters
+
+
+class ServoParamSetter(Node):
+    def __init__(self):
+        super().__init__('servo_param_setter')
+        
+        self.servo_linear_sub_ = self.create_subscription(Float64, 'servo/set_linear_scale', self.linear_callback, 10)
+        self.servo_rotational_sub_ = self.create_subscription(Float64, 'servo/set_rotational_scale', self.rotational_callback, 10)
+        
+        self.client = self.create_client(SetParameters, "/servo_node/set_parameters")
+        
+    def linear_callback(self, msg):
+        while not self.client.wait_for_service(timeout_sec=0.5):
+            self.get_logger().info("service not available, waiting again...")
+    
+        request = SetParameters.Request()
+
+        param = Parameter()
+        param.name = "moveit_servo.scale.linear"
+
+        value = ParameterValue()
+        value.type = ParameterType.PARAMETER_DOUBLE
+        value.double_value = msg.data
+        param.value = value
+
+        request.parameters = [param]
+        
+        self.client.call(request)
+        
+    def rotational_callback(self, msg):
+        while not self.client.wait_for_service(timeout_sec=0.5):
+            self.get_logger().info("service not available, waiting again...")
+    
+        request = SetParameters.Request()
+
+        param = Parameter()
+        param.name = "moveit_servo.scale.rotational"
+
+        value = ParameterValue()
+        value.type = ParameterType.PARAMETER_DOUBLE
+        value.double_value = msg.data
+        param.value = value
+
+        request.parameters = [param]
+        
+        self.client.call(request)
+        
+        
+        
+        
+        
+            
