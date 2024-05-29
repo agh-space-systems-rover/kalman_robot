27417a255667384c8dd2fa504b9b4226b18b5758
Kacper Iwicki <kacper.iwi@gmail.com>
2024-05-29 00:58:35 -0600

    Added gps info to front

--- kalman_groundstation/web_frontend/src/components/Map/MapControlPanel.tsx
+++ kalman_groundstation/web_frontend/src/components/Map/MapControlPanel.tsx
@@ -11,83 +11,88 @@ import type { MapSettings, Markers } from './Map'
 import { loadWaypoints, mapJumpTo } from './mapUtils'
 
 interface Props {
-  map: MapType
-  goalGpsPosition: { lat: number; lon: number }
-  markers: Markers
-  mapSettings: MapSettings
-  setMapSettings: React.Dispatch<React.SetStateAction<MapSettings>>
+    map: MapType
+    goalGpsPosition: { lat: number; lon: number }
+    markers: Markers
+    mapSettings: MapSettings
+    setMapSettings: React.Dispatch<React.SetStateAction<MapSettings>>
 }
 
 export const MapControlPanel: React.FC<Props> = ({ map, goalGpsPosition, markers, mapSettings, setMapSettings }) => {
-  const bleSignal = useAppSelector((state) => state.autonomy.bleSignal)
+    const bleSignal = useAppSelector((state) => state.autonomy.bleSignal)
 
-  const [id, setId] = useState<number>(0)
-  const [desc, setDesc] = useState<string>('opis')
+    const gps = useAppSelector((state) => state.autonomy.gps)
 
-  return (
-    <div className='controlPanelWrapper'>
-      <button
-        onClick={(): void => {
-          setMapSettings((prevSettings) => ({ ...prevSettings, followRover: true }))
-        }}
-      >
-        Follow rover
-      </button>
-      <button
-        onClick={(): void => {
-          setMapSettings((prevSettings) => ({ ...prevSettings, followRover: false }))
-          mapJumpTo(map, goalGpsPosition)
-        }}
-      >
-        Jump to goal
-      </button>
-      {/* ############## CIRC only feature ############## */}
-      <div style={{ display: 'none' }}>
-        <span style={{ marginLeft: 3 }}>BLE Signal: {bleSignal}</span>
-        <br />
-        <input
-          id='show-ble'
-          type='checkbox'
-          checked={mapSettings.showBleSignal}
-          onChange={(): void => {
-            setMapSettings((prevSettings) => ({ ...prevSettings, showBleSignal: !prevSettings.showBleSignal }))
-          }}
-        />
-        <label htmlFor='show-ble'>Show BLE signal</label>
-      </div>
-      {/* ############################################## */}
-      <input
-        id='show-gps-path'
-        type='checkbox'
-        checked={mapSettings.showGpsPath}
-        onChange={(): void => {
-          setMapSettings((prevSettings) => ({ ...prevSettings, showGpsPath: !prevSettings.showGpsPath }))
-        }}
-      />
-      <label htmlFor='show-gps-path'>Show GPS Path</label>
-      <div style={{ display: 'none' }}>
-        <br />
-        <input id='user-marker-id' type='number' value={id} onChange={(e): void => setId(parseInt(e.target.value))} />
-        <label htmlFor='show-gps-path'>UM ID </label>
-        <input id='user-marker-desc' type='text' value={desc} onChange={(e): void => setDesc(e.target.value)} />
-        <label htmlFor='show-gps-path'>UM Desc</label>
-        <br />
-        <button
-          onClick={(): void => {
-            logUserMark(markers.userMarkers[id].getLngLat().lat, markers.userMarkers[id].getLngLat().lng, 0, desc)
-          }}
-        >
-          Save point!
-        </button>
-      </div>
+    const [id, setId] = useState<number>(0)
+    const [desc, setDesc] = useState<string>('opis')
 
-      <button
-        onClick={(): void => {
-          loadWaypoints(map)
-        }}
-      >
-        Load waypoints
-      </button>
-    </div>
-  )
+    return (
+        <div className='controlPanelWrapper'>
+            <button
+                onClick={(): void => {
+                    setMapSettings((prevSettings) => ({ ...prevSettings, followRover: true }))
+                }}
+            >
+                Follow rover
+            </button>
+            <button
+                onClick={(): void => {
+                    setMapSettings((prevSettings) => ({ ...prevSettings, followRover: false }))
+                    mapJumpTo(map, goalGpsPosition)
+                }}
+            >
+                Jump to goal
+            </button>
+            {/* ############## CIRC only feature ############## */}
+            <div style={{ display: 'none' }}>
+                <span style={{ marginLeft: 3 }}>BLE Signal: {bleSignal}</span>
+                <br />
+                <input
+                    id='show-ble'
+                    type='checkbox'
+                    checked={mapSettings.showBleSignal}
+                    onChange={(): void => {
+                        setMapSettings((prevSettings) => ({ ...prevSettings, showBleSignal: !prevSettings.showBleSignal }))
+                    }}
+                />
+                <label htmlFor='show-ble'>Show BLE signal</label>
+            </div>
+            {/* ############################################## */}
+            <input
+                id='show-gps-path'
+                type='checkbox'
+                checked={mapSettings.showGpsPath}
+                onChange={(): void => {
+                    setMapSettings((prevSettings) => ({ ...prevSettings, showGpsPath: !prevSettings.showGpsPath }))
+                }}
+            />
+            <label htmlFor='show-gps-path'>Show GPS Path</label>
+            <div style={{ display: 'none' }}>
+                <br />
+                <input id='user-marker-id' type='number' value={id} onChange={(e): void => setId(parseInt(e.target.value))} />
+                <label htmlFor='show-gps-path'>UM ID </label>
+                <input id='user-marker-desc' type='text' value={desc} onChange={(e): void => setDesc(e.target.value)} />
+                <label htmlFor='show-gps-path'>UM Desc</label>
+                <br />
+                <button
+                    onClick={(): void => {
+                        logUserMark(markers.userMarkers[id].getLngLat().lat, markers.userMarkers[id].getLngLat().lng, 0, desc)
+                    }}
+                >
+                    Save point!
+                </button>
+            </div>
+
+            <button
+                onClick={(): void => {
+                    loadWaypoints(map)
+                }}
+            >
+                Load waypoints
+            </button>
+            <p marginLeft="5px">
+                Lattitude: {gps.latitude}, Longitude: {gps.longitude}, Altitude: {gps.altitude}
+            </p>
+        </div>
+    )
 }
