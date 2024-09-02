2287006b1a598d789ad0cea369d5866079afafe1
domin746826 <ominek.pl91@gmail.com>
2024-09-02 02:08:01 +0200

    added zeroframes limit

--- kalman_master/kalman_master/drill_driver_node.py
+++ kalman_master/kalman_master/drill_driver_node.py
@@ -7,11 +7,16 @@ from kalman_interfaces.msg import Drill, MasterMessage
 ARM_SPEED = 50
 RACK_SPEED = 50
 DRILL_SPEED = 100
+MAX_ZEROFRAMES_SPAM = 5
 
 
 class DrillDriver(Node):
     def __init__(self):
         super().__init__("drill_driver")
+
+        self.zero_arm = 0
+        self.zero_rack = 0
+        self.zero_drill = 0
 
         self.master_pub = self.create_publisher(
             MasterMessage, "master_com/ros_to_master", 10
@@ -28,15 +33,27 @@ class DrillDriver(Node):
         data_rack = [1 if msg.rack < 0 else 0, round(abs(msg.rack * RACK_SPEED))]
         data_drill = [1 if msg.drill < 0 else 0, round(abs(msg.drill * DRILL_SPEED))]
 
-        self.master_pub.publish(
-            MasterMessage(cmd=MasterMessage.DRILL_ARM, data=data_arm)
-        )
-        self.master_pub.publish(
-            MasterMessage(cmd=MasterMessage.DRILL_RACK, data=data_rack)
-        )
-        self.master_pub.publish(
-            MasterMessage(cmd=MasterMessage.DRILL_DRILL, data=data_drill)
-        )
+        self.zero_arm = self.zero_arm + 1 if self.is_zero_frame(data_arm) else 0
+        self.zero_rack = self.zero_rack + 1 if self.is_zero_frame(data_rack) else 0
+        self.zero_drill = self.zero_drill + 1 if self.is_zero_frame(data_drill) else 0
+
+        if self.zero_arm <= MAX_ZEROFRAMES_SPAM:
+            self.master_pub.publish(
+                MasterMessage(cmd=MasterMessage.DRILL_ARM, data=data_arm)
+            )
+
+        if self.zero_rack <= MAX_ZEROFRAMES_SPAM:
+            self.master_pub.publish(
+                MasterMessage(cmd=MasterMessage.DRILL_RACK, data=data_rack)
+            )
+
+        if self.zero_drill <= MAX_ZEROFRAMES_SPAM:
+            self.master_pub.publish(
+                MasterMessage(cmd=MasterMessage.DRILL_DRILL, data=data_drill)
+            )
+
+    def is_zero_frame(self, data):
+        return True if data[0] == 0 and data[1] == 0 else False
 
 
 def main():
