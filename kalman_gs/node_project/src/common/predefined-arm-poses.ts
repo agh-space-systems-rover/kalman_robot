dd8dd7331753540f81bd2d02be0a72aa1ead5433
Kacper Iwicki <69985093+kapi117@users.noreply.github.com>
2024-09-07 05:59:46 +0200

    feat: science trajectories (#39)

--- kalman_gs/node_project/src/common/predefined-arm-poses.ts
+++ kalman_gs/node_project/src/common/predefined-arm-poses.ts
@@ -58,6 +58,22 @@ const PREDEFINED_POSES = {
       'joints_set': [1, 2, 3, 4, 5, 6],
       'joints_checked': [2, 3],
       'safe_previous_poses': [0, 2, 5]
+    },
+    {
+      'id': 7,
+      'name': 'Sample storage',
+      'path': 'predefined_poses/sample_storage.yaml',
+      'joints_set': [1, 2, 3, 4, 5, 6],
+      'joints_checked': [2, 3],
+      'safe_previous_poses': [0, 2, 5]
+    },
+    {
+      'id': 8,
+      'name': 'Rock storage',
+      'path': 'predefined_poses/rock_storage.yaml',
+      'joints_set': [1, 2, 3, 4, 5, 6],
+      'joints_checked': [2, 3],
+      'safe_previous_poses': [0, 2, 5]
     }
   ]
 };
@@ -69,7 +85,9 @@ const POSES_JOINTS = {
   'Base Left': [2.2435, 0.3388, 1.7765, 0.0, 1.0902, 0.7],
   'Base Right': [-2.2435, 0.3388, 1.7765, 0.0, 1.0902, -0.7],
   'Based': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-  'Spectro approach': [0.0, 0.28, 2.4, 3.14, -1.7, 0.0]
+  'Spectro approach': [0.0, 0.28, 2.4, 3.14, -1.7, 0.0],
+  'Sample storage': [0.0, 0.0, 1.57, 0.0, 0.0, 0.0],
+  'Rock storage': [0.0, 0.0, 1.57, 0.0, 0.0, 0.0]
 };
 
 export default { PREDEFINED_POSES, POSES_JOINTS };
