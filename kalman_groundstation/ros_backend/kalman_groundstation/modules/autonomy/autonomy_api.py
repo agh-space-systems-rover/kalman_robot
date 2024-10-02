2c8acd7716fe3db047d7028a7a2625b37c0152b7
rayferric <63957587+rayferric@users.noreply.github.com>
2024-10-02 19:49:26 +0200

    remove kalman_groundstation

--- kalman_groundstation/ros_backend/kalman_groundstation/modules/autonomy/autonomy_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from rclpy.node import Node
-from .api.autonomy import AutonomyRouter
-
-class AutonomyAPI(APIRouter):
-    def __init__(self, parent_node: Node) -> None:
-        super().__init__(prefix="/autonomy", tags=["autonomy"])
-
-        self.include_router(AutonomyRouter(parent_node))
--- kalman_groundstation/ros_backend/kalman_groundstation/modules/science/science_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from rclpy.node import Node
-
-from .api.science import ScienceRouter
-
-class ScienceAPI(APIRouter):
-    def __init__(self, parent_node: Node) -> None:
-        super().__init__(prefix="/science")
-        self.include_router(ScienceRouter(parent_node))
