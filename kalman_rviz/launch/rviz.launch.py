679fe312bfb1ee6b61f7d49cabd9c3d3f087b8c5
rayferric <63957587+rayferric@users.noreply.github.com>
2024-12-01 19:40:00 +0100

    fix kalman_rviz launch

--- kalman_rviz/launch/rviz.launch.py
+++ kalman_rviz/launch/rviz.launch.py
@@ -11,8 +11,15 @@ from launch.substitutions import LaunchConfiguration
 
 def launch_setup(context):
     configs = [
-        x for x in LaunchConfiguration("config").perform(context).split(" ") if x != ""
+        x for x in LaunchConfiguration("configs").perform(context).split(" ") if x != ""
     ]
+
+    if len(configs) == 0:
+        available = [
+            x.stem for x in (get_package_share_path("kalman_rviz") / "rviz").glob("*.rviz")
+        ]
+        available = '\n - '.join(available)
+        raise RuntimeError(f"\n\nNo RViz configurations were chosen. Please set configs:=\"config1 config2 ...\". Available:\n - {available}")
 
     return [
         Node(
@@ -34,7 +41,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             DeclareLaunchArgument(
-                "config",
+                "configs",
                 default_value="",
                 description="Space separated RViz configuration file names without extensions, e.g. 'autonomy demo_rgbd'",
             ),
