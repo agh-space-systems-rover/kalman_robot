4bea1e42042aeed917e07c6eafd76834fd34afe9
rayferric <63957587+rayferric@users.noreply.github.com>
2025-01-17 21:36:31 +0100

    bringup: invalid description.layout=pc entries

--- kalman_bringup/launch/urc_pc_nav.launch.py
+++ kalman_bringup/launch/urc_pc_nav.launch.py
@@ -5,7 +5,7 @@ RGBD_IDS = "d455_front d455_back d455_left d455_right"
 def generate_launch_description():
     return gen_launch({
         "description": {
-            "layout": "pc",
+            "layout": "autonomy",
         },
         "hardware": {
             "master": "pc",
