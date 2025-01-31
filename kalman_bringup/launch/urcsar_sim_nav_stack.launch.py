027d9a51044a8a9732683da265f852c439975399
rayferric <63957587+rayferric@users.noreply.github.com>
2025-01-31 23:52:39 +0100

    URC SAR video ArUco search  launchfiles

---
+++ kalman_bringup/launch/urcsar_sim_nav_stack.launch.py
@@ -0,0 +1,35 @@
+from kalman_bringup import *
+
+RGBD_IDS = "d455_front d455_back d455_left d455_right"
+
+
+def generate_launch_description():
+    return gen_launch(
+        {
+            "description": {
+                "layout": "autonomy",
+            },
+            "clouds": {
+                "rgbd_ids": RGBD_IDS,
+            },
+            "slam": {
+                "rgbd_ids": RGBD_IDS,
+                "gps_datum": "50.06623534 19.9132241",
+                "use_mag": "true",
+            },
+            "nav2": {
+                "rgbd_ids": RGBD_IDS,
+                "static_map": "erc2024",
+            },
+            "wheels": {},
+            "aruco": {
+                "rgbd_ids": RGBD_IDS,
+                "dict": "5X5_100",
+                "size": "0.15",
+            },
+            "supervisor": {
+                "aruco_rgbd_ids": RGBD_IDS,
+                "aruco_deactivate_unused": "true",
+            },
+        }
+    )
