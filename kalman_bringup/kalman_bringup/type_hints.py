7ec9c4006b2f3da815daba231a30910536f083b7
rayferric <63957587+rayferric@users.noreply.github.com>
2025-02-24 05:43:59 +0100

    bringup: improve composition, sort generated type hints to aid version control

--- kalman_bringup/kalman_bringup/type_hints.py
+++ kalman_bringup/kalman_bringup/type_hints.py
@@ -4,42 +4,30 @@
 from typing import Literal, TypedDict
 
 
-class Yolo(TypedDict):
-    rgbd_ids: str
-    "Space-separated IDs of the depth cameras to use."
-    config: Literal["urc2024"]
-    "name of the configuration to load. Valid choices are: {'urc2024'}"
+class Arm(TypedDict):
+    debug: Literal["False", "True", "false", "true"]
+    "One of: ['true', 'false', 'True', 'False']"
+    allow_trajectory_execution: Literal["False", "True", "false", "true"]
+    "One of: ['true', 'false', 'True', 'False']"
+    publish_monitored_planning_scene: Literal["False", "True", "false", "true"]
+    "One of: ['true', 'false', 'True', 'False']"
+    monitor_dynamics: Literal["False", "True", "false", "true"]
+    "One of: ['true', 'false', 'True', 'False']"
 
 
-class UnitySim(TypedDict):
-    scene: str
-    "The scene to load in Unity."
+class ArmGs(TypedDict):
+    pass
 
 
-class Nav2(TypedDict):
+class Aruco(TypedDict):
     component_container: str
     "Name of an existing component container to use. Empty by default to disable composition."
     rgbd_ids: str
-    "Space-separated IDs of the depth cameras to use."
-    static_map: Literal["", "erc2023", "erc2024"]
-    "Name of the static map to use. Maps are stored in kalman_nav2/maps. Empty by default to disable static map. Valid choices are: ['', 'erc2024', 'erc2023']"
-
-
-class Slam(TypedDict):
-    component_container: str
-    "Name of an existing component container to use. Empty to disable composition."
-    rgbd_ids: str
-    "Space-separated IDs of the depth cameras to use."
-    gps_datum: str
-    "The 'latitude longitude' of the map frame. Only used if GPS is enabled. Empty to assume first recorded GPS fix."
-    fiducials: Literal["", "erc2024", "terc2024"]
-    "Name of the list of fiducials to use. Empty disables fiducial odometry. Valid choices are: ['', 'erc2024', 'terc2024']"
-    use_mag: Literal["false", "true"]
-    "Use IMU yaw readings for global EKF. If disabled, heading will drift over time. Valid choices are: ['true', 'false']"
-
-
-class Gs(TypedDict):
-    pass
+    "Space-separated IDs of the RGBD cameras to use. Regular cameras are not supported yet."
+    dict: Literal["4X4_100", "4X4_1000", "4X4_250", "4X4_50", "5X5_100", "5X5_1000", "5X5_250", "5X5_50", "6X6_100", "6X6_1000", "6X6_250", "6X6_50", "7X7_100", "7X7_1000", "7X7_250", "7X7_50", "8X8_100", "8X8_1000", "8X8_250", "8X8_50", "ARUCO_ORIGINAL"]
+    "Dictionary of markers to use. Valid choices are: ['4X4_50', '4X4_100', '4X4_250', '4X4_1000', '5X5_50', '5X5_100', '5X5_250', '5X5_1000', '6X6_50', '6X6_100', '6X6_250', '6X6_1000', '7X7_50', '7X7_100', '7X7_250', '7X7_1000', '8X8_50', '8X8_100', '8X8_250', '8X8_1000', 'ARUCO_ORIGINAL']"
+    size: str
+    "Size of the marker including black border in meters."
 
 
 class Clouds(TypedDict):
@@ -49,37 +37,14 @@ class Clouds(TypedDict):
     "Space-separated IDs of the depth cameras to use for point cloud generation."
 
 
-class Wheels(TypedDict):
-    joy: Literal["", "arduino", "gamepad"]
-    "Joy device to use for headless driving. Choose 'gamepad' or 'arduino'. Empty disables headless teleop. Valid choices are: ['', 'gamepad', 'arduino']"
+class Description(TypedDict):
+    layout: Literal["arm", "autonomy"]
+    "layout of the robot: autonomy, arm. Valid choices are: ['autonomy', 'arm']"
+    joint_state_publisher_gui: Literal["false", "true"]
+    "Start the joint state publisher in GUI mode. Valid choices are: ['true', 'false']"
 
 
-class Supervisor(TypedDict):
-    aruco_rgbd_ids: str
-    "Space-separated IDs of the depth cameras that were configured in kalman_aruco."
-    aruco_deactivate_unused: Literal["false", "true"]
-    "Deactivate ArUco detection nodes when supervisor is not actively looking for tags. Valid choices are: ['true', 'false']"
-    yolo_enabled: Literal["false", "true"]
-    "Whether YOLO detection is enabled. Valid choices are: ['true', 'false']"
-    yolo_deactivate_unused: Literal["false", "true"]
-    "Deactivate YOLO detection when supervisor is not actively looking for objects. Valid choices are: ['true', 'false']"
-
-
-class Spacenav(TypedDict):
-    pass
-
-
-class Rviz(TypedDict):
-    configs: str
-    "Space separated RViz configuration file names without extensions, e.g. 'autonomy demo_rgbd'"
-
-
-class Master(TypedDict):
-    mode: Literal["arm", "gs", "pc"]
-    "On what hardware is this module being run? Available modes: gs, pc, arm. Valid choices are: ['gs', 'pc', 'arm']"
-
-
-class ArmGs(TypedDict):
+class Gs(TypedDict):
     pass
 
 
@@ -98,64 +63,103 @@ class Hardware(TypedDict):
     "Start the GPS driver. Valid choices are: ['true', 'false']"
 
 
-class Description(TypedDict):
-    layout: Literal["arm", "autonomy"]
-    "layout of the robot: autonomy, arm. Valid choices are: ['autonomy', 'arm']"
-    joint_state_publisher_gui: Literal["false", "true"]
-    "Start the joint state publisher in GUI mode. Valid choices are: ['true', 'false']"
+class Master(TypedDict):
+    mode: Literal["arm", "gs", "pc"]
+    "On what hardware is this module being run? Available modes: gs, pc, arm. Valid choices are: ['gs', 'pc', 'arm']"
 
 
-class Aruco(TypedDict):
+class Nav2(TypedDict):
     component_container: str
     "Name of an existing component container to use. Empty by default to disable composition."
     rgbd_ids: str
-    "Space-separated IDs of the RGBD cameras to use. Regular cameras are not supported yet."
-    dict: Literal["4X4_100", "4X4_1000", "4X4_250", "4X4_50", "5X5_100", "5X5_1000", "5X5_250", "5X5_50", "6X6_100", "6X6_1000", "6X6_250", "6X6_50", "7X7_100", "7X7_1000", "7X7_250", "7X7_50", "8X8_100", "8X8_1000", "8X8_250", "8X8_50", "ARUCO_ORIGINAL"]
-    "Dictionary of markers to use. Valid choices are: ['4X4_50', '4X4_100', '4X4_250', '4X4_1000', '5X5_50', '5X5_100', '5X5_250', '5X5_1000', '6X6_50', '6X6_100', '6X6_250', '6X6_1000', '7X7_50', '7X7_100', '7X7_250', '7X7_1000', '8X8_50', '8X8_100', '8X8_250', '8X8_1000', 'ARUCO_ORIGINAL']"
-    size: str
-    "Size of the marker including black border in meters."
+    "Space-separated IDs of the depth cameras to use."
+    static_map: Literal["", "erc2023", "erc2024"]
+    "Name of the static map to use. Maps are stored in kalman_nav2/maps. Empty by default to disable static map. Valid choices are: ['', 'erc2023', 'erc2024']"
 
 
-class Arm(TypedDict):
-    debug: Literal["False", "True", "false", "true"]
-    "One of: ['true', 'false', 'True', 'False']"
-    allow_trajectory_execution: Literal["False", "True", "false", "true"]
-    "One of: ['true', 'false', 'True', 'False']"
-    publish_monitored_planning_scene: Literal["False", "True", "false", "true"]
-    "One of: ['true', 'false', 'True', 'False']"
-    monitor_dynamics: Literal["False", "True", "false", "true"]
-    "One of: ['true', 'false', 'True', 'False']"
+class Rviz(TypedDict):
+    configs: str
+    "Space separated RViz configuration file names without extensions, e.g. 'autonomy demo_rgbd'"
+
+
+class Slam(TypedDict):
+    component_container: str
+    "Name of an existing component container to use. Empty to disable composition."
+    rgbd_ids: str
+    "Space-separated IDs of the depth cameras to use."
+    gps_datum: str
+    "The 'latitude longitude' of the map frame. Only used if GPS is enabled. Empty to assume first recorded GPS fix."
+    fiducials: Literal["", "erc2024", "terc2024"]
+    "Name of the list of fiducials to use. Empty disables fiducial odometry. Valid choices are: ['', 'terc2024', 'erc2024']"
+    use_mag: Literal["false", "true"]
+    "Use IMU yaw readings for global EKF. If disabled, heading will drift over time. Valid choices are: ['true', 'false']"
+
+
+class Spacenav(TypedDict):
+    pass
+
+
+class Supervisor(TypedDict):
+    aruco_rgbd_ids: str
+    "Space-separated IDs of the depth cameras that were configured in kalman_aruco."
+    aruco_deactivate_unused: Literal["false", "true"]
+    "Deactivate ArUco detection nodes when supervisor is not actively looking for tags. Valid choices are: ['true', 'false']"
+    yolo_enabled: Literal["false", "true"]
+    "Whether YOLO detection is enabled. Valid choices are: ['true', 'false']"
+    yolo_deactivate_unused: Literal["false", "true"]
+    "Deactivate YOLO detection when supervisor is not actively looking for objects. Valid choices are: ['true', 'false']"
+
+
+class UnitySim(TypedDict):
+    component_container: str
+    "Name of an existing component container to use. Empty to disable composition."
+    scene: str
+    "The scene to load in Unity."
+    selective_launch: Literal["all", "only_ros", "only_sim"]
+    "Selectively launch just Unity or just the ROS nodes. Valid choices are: ['all', 'only_sim', 'only_ros']"
+
+
+class Wheels(TypedDict):
+    joy: Literal["", "arduino", "gamepad"]
+    "Joy device to use for headless driving. Choose 'gamepad' or 'arduino'. Empty disables headless teleop. Valid choices are: ['', 'gamepad', 'arduino']"
+
+
+class Yolo(TypedDict):
+    rgbd_ids: str
+    "Space-separated IDs of the depth cameras to use."
+    config: Literal["urc2024"]
+    "name of the configuration to load. Valid choices are: {'urc2024'}"
 
 
 class BringupConfig(TypedDict):
-    yolo: Yolo
-    "models and configs for `yolo_ros`"
-    unity_sim: UnitySim
-    nav2: Nav2
-    "configuration and launch files for Nav2 and related modules; Includes a custom path follower."
-    slam: Slam
-    "configuration + launch files for robot_localization and RTAB-Map"
-    gs: Gs
-    "ReactJS-based GUI for the rover"
-    clouds: Clouds
-    "point cloud generation and filtering"
-    wheels: Wheels
-    "controller node that converts Twist messages on `/cmd_vel` and similar topics to the actual wheel state; Also includes safeguards that can limit the acceleration and velocity or stop the rover to adjust wheel rotation."
-    supervisor: Supervisor
-    "Manages autonomous navigation missions."
-    spacenav: Spacenav
-    "Launch files and scripts for the SpaceMouse."
-    rviz: Rviz
-    "Launch files and configs for RViz"
-    master: Master
-    "driver for our custom Master device"
-    arm_gs: ArmGs
-    "scripts for gs site for interfacing with arm"
-    hardware: Hardware
-    "drivers, tools and launch scripts for the physical hardware onboard; Only to be run separately from the simulation on a physical robot."
-    description: Description
-    "Xacro / URDF descriptions + models for the rover"
-    aruco: Aruco
-    "pre-configured setup for aruco_opencv"
     arm: Arm
     "configuration and launch files for the arm"
+    arm_gs: ArmGs
+    "scripts for gs site for interfacing with arm"
+    aruco: Aruco
+    "pre-configured setup for aruco_opencv"
+    clouds: Clouds
+    "point cloud generation and filtering"
+    description: Description
+    "Xacro / URDF descriptions + models for the rover"
+    gs: Gs
+    "ReactJS-based GUI for the rover"
+    hardware: Hardware
+    "drivers, tools and launch scripts for the physical hardware onboard; Only to be run separately from the simulation on a physical robot."
+    master: Master
+    "driver for our custom Master device"
+    nav2: Nav2
+    "configuration and launch files for Nav2 and related modules; Includes a custom path follower."
+    rviz: Rviz
+    "Launch files and configs for RViz"
+    slam: Slam
+    "configuration + launch files for robot_localization and RTAB-Map"
+    spacenav: Spacenav
+    "Launch files and scripts for the SpaceMouse."
+    supervisor: Supervisor
+    "Manages autonomous navigation missions."
+    unity_sim: UnitySim
+    wheels: Wheels
+    "controller node that converts Twist messages on `/cmd_vel` and similar topics to the actual wheel state; Also includes safeguards that can limit the acceleration and velocity or stop the rover to adjust wheel rotation."
+    yolo: Yolo
+    "models and configs for `yolo_ros`"
