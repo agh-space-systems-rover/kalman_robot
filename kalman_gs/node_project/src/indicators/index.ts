3dcc4c815c6c858620f3654e233f70df9a5f8296
rayferric <63957587+rayferric@users.noreply.github.com>
2025-03-05 03:11:10 +0100

    interstellar

--- kalman_gs/node_project/src/indicators/index.ts
+++ kalman_gs/node_project/src/indicators/index.ts
@@ -1,13 +1,9 @@
 import GamepadSelector from './gamepad-selector';
+import Interstellar from './interstellar';
 import OpenSettings from './open-settings';
 import RebootPC from './reboot-pc';
 import RosHealth from './ros-health';
 import ThemeSelector from './theme-selector';
 
 // Add new indicators here:
-export const IndicatorComponents = [
-  GamepadSelector,
-  RebootPC,
-  RosHealth,
-  OpenSettings
-];
+export const IndicatorComponents = [Interstellar, GamepadSelector, RebootPC, RosHealth, OpenSettings];
