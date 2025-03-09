16aac37eac43acd3e638220cddbbf3576a27d4d3
rayferric <63957587+rayferric@users.noreply.github.com>
2025-03-09 14:09:51 +0100

    add arch package and yolo to launch

--- kalman_bringup/launch/arch_pc_nav.launch.py
+++ kalman_bringup/launch/arch_pc_nav.launch.py
@@ -23,7 +23,14 @@ def generate_launch_description():
             },
             "wheels": {
                 "joy": "gamepad",
-            }
+            },
+            "arch": {
+                "rgbd_ids": RGBD_IDS,                
+            },
+            "yolo": {
+                "rgbd_ids": RGBD_IDS,
+                "config": "arch2025",
+            },
         },
         composition="start_container",
     )
