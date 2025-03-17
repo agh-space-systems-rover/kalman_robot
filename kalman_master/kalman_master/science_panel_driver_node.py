63af87c9dc3f1b508ac4ab7ce4407211834150b5
Michał Proć <michalproc4@gmail.com>
2025-03-17 21:12:43 +0100

    [WiP] Science Panel

--- kalman_master/kalman_master/science_panel_driver_node.py
+++ kalman_master/kalman_master/science_panel_driver_node.py
@@ -1,24 +1,56 @@
+from enum import Enum
 import yaml
-
 import rclpy
-
 from rclpy.node import Node
+from kalman_interfaces.msg import MasterMessage, GetScienceElements, ScienceElement, ScienceButton
 
-from kalman_interfaces.msg import MasterMessage
+
+class ScienceElementType(Enum):
+    DISPLAY = 0
+    CONTAINER = 1
+    PLAYER = 2
 
 
 class SciencePanelDriver(Node):
     def __init__(self):
-        super().__init__("ueuos_driver")
+        super().__init__("science_panel_driver")
         filename = self.declare_parameter("config_path").value
 
         with open(filename, "r") as f:
-            self.panels_list = yaml.safe_load(f)
+            self.science_config = yaml.safe_load(f)
 
         # Init master publisher.
         self.ueuos_pub = self.create_publisher(
             MasterMessage, "master_com/ros_to_master", 10
         )
+
+        # Create service to serve configuration
+        self.create_service(GetScienceElements, "science_panel/get_config", self.get_science_elements)
+
+    def get_science_elements(self, request: GetScienceElements.Request, response: GetScienceElements.Response):
+        elements = []
+
+        for element in self.science_config.get("elements", []):
+            science_element_buttons = []
+            for button in self.science_config.get("buttons", []):
+                science_element_buttons.append(ScienceButton(
+                    id=button.get("id"),
+                    name=button.get("name"),
+                    color=button.get("color"),
+                ))
+
+            elements.append(ScienceElement(
+                id=element.get("id"),
+                type=eval(element.get("type")),
+                display_name=element.get("display_name"),
+                buttons=science_element_buttons,
+                color=element.get("type_data").get("color"),
+            ))
+
+        return {
+            "enable": self.science_config.get("enable", False),
+            "science_elements": elements,
+        }
 
 
 def main():
