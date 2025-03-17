bb7c90e83c334d0dee7bc8aa9070c42874972b0e
Michał Proć <michalproc4@gmail.com>
2025-03-17 21:12:43 +0100

    [WiP] Science Panel

--- kalman_gs/node_project/src/panels/science.tsx
+++ kalman_gs/node_project/src/panels/science.tsx
@@ -1,13 +1,59 @@
-import styles from './ueuos.module.css';
+import styles from './science.module.css';
 
-import React, { useRef, useState } from 'react';
 
-type Props = {
-  props: {};
+
+import { ros } from '../common/ros';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Service } from 'roslib';
+
+
+enum ScienceElementType {
+  DISPLAY = 0,
+  CONTAINER = 1,
+  PLAYER = 2
+}
+
+type ScienceElements = {
+  enable?: boolean;
+  science_elements?: ScienceElement[];
 };
 
-window.addEventListener('ros-connect', () => {});
+type ScienceElement = {
+  id?: string;
+  type?: ScienceElementType;
+  display_name?: string;
+  buttons?: ScienceButton[];
+  color?: string;
+};
 
-export default function Ueuos({ props }: Props) {
+type ScienceButton = {
+  id?: string;
+  name?: string;
+  color?: string;
+};
+
+// Services:
+let scienceElementsService: Service<{}, ScienceElements> = null;
+
+window.addEventListener('ros-connect', () => {
+  scienceElementsService = new Service({
+    ros: ros,
+    name: '/science_panel/get_config',
+    serviceType: 'kalman_interfaces/GetScienceElements'
+  });
+});
+
+export default function Ueuos() {
+  const [scienceElements, setScienceElements] = useState(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (scienceElementsService !== null) {
+        scienceElementsService.callService({}, (data: ScienceElements) => console.log(data), (err) => console.log(err));
+
+      }
+    }, 1000)
+  }, []);
+
   return <div className={styles['science']}></div>;
 }
