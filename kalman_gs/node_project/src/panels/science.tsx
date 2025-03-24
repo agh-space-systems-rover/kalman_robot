d0e7c73a885c8549f2ec72b97707848035274991
Michał Proć <michalproc4@gmail.com>
2025-03-24 07:40:42 +0100

    Science Panel: complete logic w/o Display and parsers

--- kalman_gs/node_project/src/panels/science.tsx
+++ kalman_gs/node_project/src/panels/science.tsx
@@ -2,10 +2,22 @@ import styles from './science.module.css';
 
 import { alertsRef } from '../common/refs';
 import { ros } from '../common/ros';
-import { faCircleExclamation, faRefresh } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBox,
+  faBoxOpen,
+  faCircleExclamation,
+  faRefresh,
+  faPlay,
+  faStop,
+  faPause,
+  faCircle
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useCallback, useEffect, useState } from 'react';
-import { Service } from 'roslib';
+import { Service, Topic } from 'roslib';
+
+import Button from '../components/button';
+import Input from '../components/input';
 
 enum ScienceElementType {
   DISPLAY = 0,
@@ -13,6 +25,15 @@ enum ScienceElementType {
   PLAYER = 2,
   STATUS = 3
 }
+
+type ScienceElementProps = {
+  element: ScienceElement;
+};
+
+type ScienceButtonsProps = {
+  parent_id: string;
+  buttons: ScienceButton[];
+};
 
 type ScienceElements = {
   enable?: boolean;
@@ -42,22 +63,191 @@ window.addEventListener('ros-connect', () => {
     name: '/science_panel/get_config',
     serviceType: 'kalman_interfaces/GetScienceElements'
   });
-
   window.dispatchEvent(new Event('able-to-request-science-elements'));
 });
 
+function Display({ element }: ScienceElementProps) {
+  {
+    element.display_name && <h2 className={styles['science-element-header']}>{element.display_name}</h2>;
+  }
+
+  return (
+    <div className={styles['science-element']}>
+      {element.buttons && <Buttons parent_id={element.id} buttons={element.buttons} />}
+      <div className={styles['row']}>{/*<Input dis*/}</div>
+    </div>
+  );
+}
+
+function Container({ element }: ScienceElementProps) {
+  const openTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_open_container`,
+    messageType: 'std_msgs/Empty'
+  });
+
+  const closeTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_close_container`,
+    messageType: 'std_msgs/Empty'
+  });
+
+  const styleSheet = getComputedStyle(document.body);
+  const containerStyle = element.color ? { color: styleSheet.getPropertyValue(`--${element.color}`) } : {};
+
+  return (
+    <div className={styles['science-element']}>
+      {element.display_name && <h2 className={styles['science-element-header']}>{element.display_name}</h2>}
+      {element.buttons && <Buttons parent_id={element.id} buttons={element.buttons} />}
+      <div>
+        <FontAwesomeIcon
+          icon={faBoxOpen}
+          onClick={() => openTopic.publish({})}
+          style={containerStyle}
+          className={styles['science-element-icon']}
+        />
+        <FontAwesomeIcon
+          icon={faBox}
+          onClick={() => closeTopic.publish({})}
+          style={containerStyle}
+          className={styles['science-element-icon']}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Player({ element }: ScienceElementProps) {
+  const playTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_player_play`,
+    messageType: 'std_msgs/Empty'
+  });
+
+  const stopTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_player_stop`,
+    messageType: 'std_msgs/Empty'
+  });
+
+  const pauseTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_player_pause`,
+    messageType: 'std_msgs/Empty'
+  });
+
+  const styleSheet = getComputedStyle(document.body);
+  const playerStyle = element.color ? { color: styleSheet.getPropertyValue(`--${element.color}`) } : {};
+
+  return (
+    <div className={styles['science-element']}>
+      {element.display_name && <h2 className={styles['science-element-header']}>{element.display_name}</h2>}
+      {element.buttons && <Buttons parent_id={element.id} buttons={element.buttons} />}
+      <div>
+        <FontAwesomeIcon
+          icon={faPlay}
+          onClick={() => playTopic.publish({})}
+          style={playerStyle}
+          className={styles['science-element-icon']}
+        />
+        <FontAwesomeIcon
+          icon={faStop}
+          onClick={() => stopTopic.publish({})}
+          style={playerStyle}
+          className={styles['science-element-icon']}
+        />
+        <FontAwesomeIcon
+          icon={faPause}
+          onClick={() => pauseTopic.publish({})}
+          style={playerStyle}
+          className={styles['science-element-icon']}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Status({ element }: ScienceElementProps) {
+  const statusTopic = new Topic({
+    ros: ros,
+    name: `/science_panel/${element.id}_status`,
+    messageType: 'std_msgs/Bool'
+  });
+
+  const [status, setStatus] = useState<boolean | null>(null);
+  const [timestamp, setTimestamp] = useState<string>('');
+
+  const styleSheet = getComputedStyle(document.body);
+  const defaultColor = styleSheet.getPropertyValue('--dark-background') || '#222';
+  const greenColor = 'rgb(102, 178, 51)';
+  const redColor = 'rgb(255, 102, 102)';
+
+  let iconColor = defaultColor;
+  if (status !== null) {
+    iconColor = status ? greenColor : redColor;
+  }
+
+  useEffect(() => {
+    const callback = (msg: { data: boolean }) => {
+      setStatus(msg.data);
+      const now = new Date();
+      setTimestamp(now.toLocaleTimeString()); // Format: HH:MM:SS
+    };
+
+    statusTopic.subscribe(callback);
+    return () => {
+      statusTopic.unsubscribe(callback);
+    };
+  }, [element.id]);
+
+  return (
+    <div className={styles['science-element']}>
+      {element.display_name && <h2 className={styles['science-element-header']}>{element.display_name}</h2>}
+      <div className={styles['science-element-icon-container']}>
+        <FontAwesomeIcon icon={faCircle} style={{ color: iconColor }} className={styles['science-element-icon']} />
+        {timestamp && <p className={styles['science-element-icon-text']}>{timestamp}</p>}
+      </div>
+    </div>
+  );
+}
+
+function Buttons({ parent_id, buttons }: ScienceButtonsProps) {
+  const style = getComputedStyle(document.body);
+
+  return (
+    <div className={styles['row']}>
+      {buttons.map((button) => {
+        let buttonStyle = {};
+        if (button.color) buttonStyle = { backgroundColor: style.getPropertyValue(`--${button.color}`) };
+
+        const topic = new Topic({
+          ros: ros,
+          name: `/science_panel/${parent_id}_${button.id}`,
+          messageType: 'std_msgs/Empty'
+        });
+
+        return (
+          <Button key={button.id} style={buttonStyle} onClick={() => topic.publish({})}>
+            {button.name}
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
+
 export default function Science() {
-  const [scienceElements, setScienceElements] = useState(null);
+  const [scienceElements, setScienceElements] = useState<ScienceElements | null>(null);
+
+  const handleScienceElements = useCallback((data: ScienceElements) => {
+    setScienceElements(data);
+  }, []);
 
   const requestScienceElements = useCallback(() => {
     if (scienceElementsService !== null) {
-      scienceElementsService.callService(
-        {},
-        (data: ScienceElements) => setScienceElements(data),
-        (err) => console.log(err)
-      );
+      scienceElementsService.callService({}, (data: ScienceElements) => handleScienceElements(data), undefined);
     }
-  }, [setScienceElements]);
+  }, [handleScienceElements]);
 
   useEffect(() => {
     requestScienceElements();
@@ -72,15 +262,6 @@ export default function Science() {
       <div className={styles['science']}>
         <FontAwesomeIcon icon={faCircleExclamation} className={styles['science-danger-exclamation-icon']} />
         <h1>Waiting for service...</h1>
-      </div>
-    );
-  }
-
-  if (!scienceElements.enable) {
-    return (
-      <div className={styles['science']}>
-        <FontAwesomeIcon icon={faCircleExclamation} className={styles['science-danger-exclamation-icon']} />
-        <h1>Science panel is disabled.</h1>
         <FontAwesomeIcon
           icon={faRefresh}
           className={styles['science-danger-refresh-icon']}
@@ -90,5 +271,29 @@ export default function Science() {
     );
   }
 
-  return <div className={styles['science']}></div>;
+  if (!scienceElements.enable) {
+    return (
+      <div className={styles['science']}>
+        <FontAwesomeIcon icon={faCircleExclamation} className={styles['science-danger-exclamation-icon']} />
+        <h1>Science panel is disabled.</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles['science']}>
+      {scienceElements.science_elements?.map((elem) => {
+        switch (elem.type) {
+          case ScienceElementType.DISPLAY:
+            return <Display key={elem.id} element={elem} />;
+          case ScienceElementType.CONTAINER:
+            return <Container key={elem.id} element={elem} />;
+          case ScienceElementType.PLAYER:
+            return <Player key={elem.id} element={elem} />;
+          case ScienceElementType.STATUS:
+            return <Status key={elem.id} element={elem} />;
+        }
+      })}
+    </div>
+  );
 }
--- kalman_master/kalman_master/science_panel_driver_node.py
+++ kalman_master/kalman_master/science_panel_driver_node.py
@@ -2,6 +2,7 @@ from enum import Enum
 import yaml
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String, Bool
 from example_interfaces.msg import Empty
 from kalman_interfaces.msg import MasterMessage, ScienceElement, ScienceButton
 from kalman_interfaces.srv import GetScienceElements
@@ -31,9 +32,11 @@ class SciencePanelDriver(Node):
         self.create_service(GetScienceElements, "science_panel/get_config", self.get_science_elements)
 
         # Create topic arrays
-        button_list = []
+        display_list = []
         container_list = []
         player_list = []
+        status_list = []
+        button_list = []
         for element in self.science_config.get("elements", []):
             parent_id = element.get("id")
             for button in element.get("buttons", []):
@@ -43,7 +46,14 @@ class SciencePanelDriver(Node):
                     "data": button.get("data", None)
                 })
 
-            if eval(element.get("type")).value is ScienceElementType.CONTAINER:
+            if eval(element.get("type")).value is ScienceElementType.DISPLAY.value:
+                display_list.append({
+                    "master_topic": element.get("type_data").get("topic", None),
+                    "topic": f"{parent_id}_display",
+                    "parser": eval(element.get("type_data").get("parser")),
+                })
+
+            if eval(element.get("type")).value is ScienceElementType.CONTAINER.value:
                 container_list.append({
                     "topic": f"{parent_id}_open_container",
                     "message": eval(element.get("type_data").get("open_message")),
@@ -56,7 +66,7 @@ class SciencePanelDriver(Node):
                     "data": element.get("type_data").get("close_data", None)
                 })
 
-            if eval(element.get("type")).value is ScienceElementType.PLAYER:
+            if eval(element.get("type")).value is ScienceElementType.PLAYER.value:
                 player_list.append({
                     "topic": f"{parent_id}_player_play",
                     "message": eval(element.get("type_data").get("play_message")),
@@ -75,11 +85,25 @@ class SciencePanelDriver(Node):
                     "data": element.get("type_data").get("stop_data", None)
                 })
 
-        # Create topics with empty data
-        all_topics = button_list + container_list + player_list
-        for topic_data in all_topics:
+            if eval(element.get("type")).value is ScienceElementType.STATUS.value:
+                status_list.append({
+                    "master_topic": element.get("type_data").get("topic", None),
+                    "topic": f"{parent_id}_status",
+                    "parser": eval(element.get("type_data").get("parser")),
+                })
+
+        # Create topics sending data to frontend
+        data_topics = display_list + status_list
+        for topic_data in data_topics:
             self.create_subscription(
-                Empty, f"science_panel/{topic_data.get('topic')}", self.handle_element_topic(topic_data), 10
+                MasterMessage, topic_data.get("master_topic"), self.handle_data_topic(topic_data), 10
+            )
+
+        # Create topics with empty data
+        empty_topics = button_list + container_list + player_list
+        for topic_data in empty_topics:
+            self.create_subscription(
+                Empty, f"science_panel/{topic_data.get('topic')}", self.handle_empty_topic(topic_data), 10
             )
 
     def get_science_elements(self, request: GetScienceElements.Request, response: GetScienceElements.Response):
@@ -87,7 +111,7 @@ class SciencePanelDriver(Node):
 
         for element in self.science_config.get("elements", []):
             science_element_buttons = []
-            for button in self.science_config.get("buttons", []):
+            for button in element.get("buttons", []):
                 science_element_buttons.append(ScienceButton(
                     id=button.get("id"),
                     name=button.get("name"),
@@ -107,9 +131,19 @@ class SciencePanelDriver(Node):
 
         return response
 
-    def handle_element_topic(self, topic_data: dict):
+    def handle_data_topic(self, topic_data: dict):
+        def callback(msg: MasterMessage):
+            topic_data.get("message")(msg.data, topic_data)
+
+        return callback
+
+    def handle_empty_topic(self, topic_data: dict):
         def callback(msg: Empty):
-            self.get_logger().info(f"Received on topic: {topic_data['topic']} with data: {topic_data['data']}")
+            message = MasterMessage(
+                cmd=topic_data.get("message"),
+                data=topic_data.get("data"),
+            )
+            self.science_pub.publish(message)
 
         return callback
 
@@ -123,3 +157,26 @@ def main():
         rclpy.shutdown()
     except KeyboardInterrupt:
         pass
+
+
+### PARSER FUNCTIONS
+def send_weight(data, topic_data, node):
+    publisher = node.create_publisher(
+        String,
+        f"science_panel/{topic_data.get('topic')}",
+        10
+    )
+    msg = String()
+    msg.data = str(data)
+    publisher.publish(msg)
+
+
+def send_status(data, topic_data, node):
+    publisher = node.create_publisher(
+        Bool,
+        f"science_panel/{topic_data.get('topic')}",
+        10
+    )
+    msg = Bool()
+    msg.data = bool(data)
+    publisher.publish(msg)
