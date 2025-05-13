492fd2b619f86c4c539fe058f13ad62d4896b05a
Michał Proć <michalproc4@gmail.com>
2025-05-13 02:05:14 +0200

    New package for Kalman Telegram Bot

---
+++ kalman_telegram/launch/telegram.launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def launch_setup(context):
+    pass
+
+
+def generate_launch_description():
+    pass
