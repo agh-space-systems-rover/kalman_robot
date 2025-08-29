06e3ddd4ebe504e4341d696669c68624ded2000c
Wojciech Wiśniewski <wojciech@funsafe.xyz>
2025-08-29 23:39:01 +0200

    kalman_bringup: add erc_pc_maintenance.launch.py

---
+++ kalman_bringup/launch/erc_pc_maintenance.launch.py
@@ -0,0 +1,23 @@
+from kalman_bringup import *
+
+
+def generate_launch_description():
+    return gen_launch(
+        {
+            "description": {
+                "layout": "autonomy",
+            },
+            "hardware": {
+                "master": "pc",
+                "rgbd_ids": "d435_arm",
+                "imu": "full",
+            },
+            "aruco":{
+                "rgbd_ids": "d435_arm",
+                "dict": "5X5_50",
+                "size": "0.06",
+            },
+            "wheels": {},
+            "arm2": {},
+        }
+    )
