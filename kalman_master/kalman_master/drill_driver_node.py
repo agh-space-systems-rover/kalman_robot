dbb51f51dc9287ee6a3f13e5728fbff7c24b4811
Mateuszzx <mateuszwojcikv@@gmail.com>
2026-02-01 17:36:17 +0100

    Adjust control input of developed for ERC drill UI with new frames for Universal development

--- kalman_master/kalman_master/drill_driver_node.py
+++ kalman_master/kalman_master/drill_driver_node.py
@@ -4,7 +4,6 @@ import numpy as np
 from struct import pack
 from kalman_interfaces.msg import Drill, MasterMessage
 
-ARM_SPEED = 50
 RACK_SPEED = 50
 DRILL_SPEED = 100
 MAX_ZEROFRAMES_SPAM = 5
@@ -13,8 +12,7 @@ MAX_ZEROFRAMES_SPAM = 5
 class DrillDriver(Node):
     def __init__(self):
         super().__init__("drill_driver")
-
-        self.zero_arm = 0
+        print("Drill driver node started")
         self.zero_rack = 0
         self.zero_drill = 0
 
@@ -25,35 +23,29 @@ class DrillDriver(Node):
         self.create_subscription(Drill, "drill", self.drill_data, 1)
 
     def drill_data(self, msg: Drill):
-        msg.arm = np.clip(msg.arm, -1, 1)
         msg.rack = np.clip(msg.rack, -1, 1)
-        msg.drill = np.clip(msg.drill, -1, 1)
+        msg.drill = np.clip(msg.drill, 0, 1)
 
-        data_arm = [1 if msg.arm < 0 else 0, round(abs(msg.arm * ARM_SPEED))]
-        data_rack = [1 if msg.rack < 0 else 0, round(abs(msg.rack * RACK_SPEED))]
-        data_drill = [1 if msg.drill < 0 else 0, round(abs(msg.drill * DRILL_SPEED))]
-
-        self.zero_arm = self.zero_arm + 1 if self.is_zero_frame(data_arm) else 0
+        data_rack = [0, 0, round(abs(msg.rack * RACK_SPEED)), 1 if msg.rack < 0 else 0]
+        data_drill = [0, 0, round(abs(msg.drill * DRILL_SPEED))]
         self.zero_rack = self.zero_rack + 1 if self.is_zero_frame(data_rack) else 0
         self.zero_drill = self.zero_drill + 1 if self.is_zero_frame(data_drill) else 0
 
-        if self.zero_arm <= MAX_ZEROFRAMES_SPAM:
-            self.master_pub.publish(
-                MasterMessage(cmd=MasterMessage.DRILL_ARM, data=data_arm)
-            )
-
+        # self.get_logger().info(f"Drill_zero {self.zero_rack}, Drill_zero {self.zero_drill}")
         if self.zero_rack <= MAX_ZEROFRAMES_SPAM:
+            # self.get_logger().info(f"Publishing drill data: {data_drill}")
             self.master_pub.publish(
                 MasterMessage(cmd=MasterMessage.DRILL_RACK, data=data_rack)
             )
 
         if self.zero_drill <= MAX_ZEROFRAMES_SPAM:
+            # self.get_logger().info(f"Publishing drill data: {data_drill}")
             self.master_pub.publish(
                 MasterMessage(cmd=MasterMessage.DRILL_DRILL, data=data_drill)
             )
 
     def is_zero_frame(self, data):
-        return True if data[0] == 0 and data[1] == 0 else False
+        return True if data[2] == 0 else False
 
 
 def main():
