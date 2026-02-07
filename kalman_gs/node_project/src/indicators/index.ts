ae6f13c47b6a326bdc573ae533489ab2ade2bc5e
Michał Proć <michalproc4@gmail.com>
2026-02-07 17:58:54 +0100

    Add refresh button (for Electron usage)

--- kalman_gs/node_project/src/indicators/index.ts
+++ kalman_gs/node_project/src/indicators/index.ts
@@ -5,10 +5,12 @@ import Interstellar from './interstellar';
 import OpenSettings from './open-settings';
 import RebootPC from './reboot-pc';
 import RosHealth from './ros-health';
+import RefreshButton from './refresh-button';
 
 // Add new indicators here:
 export const IndicatorComponents = [
   Interstellar,
+  RefreshButton,
   EStopButton,
   GamepadSelector,
   RebootPC,
