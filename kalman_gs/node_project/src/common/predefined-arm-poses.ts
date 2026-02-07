ee9acb1c31fb485c203c33822bac37e8019a7794
Kacpron123 <kacper030904@gmail.com>
2026-02-07 11:14:22 +0100

    deleted old poses

--- kalman_gs/node_project/src/common/predefined-arm-poses.ts
+++ kalman_gs/node_project/src/common/predefined-arm-poses.ts
@@ -13,76 +13,12 @@ const PREDEFINED_POSES = {
     },
     {
       'id': 1,
-      'name': 'Reset 4 and 6',
-      'path': 'predefined_poses/reset.yaml',
-      'joints': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-      'joints_set': [4, 6],
-      'joints_checked': [],
-      'joints_reversed': [5],
-      'safe_previous_poses': []
-    },
-    {
-      'id': 2,
-      'name': 'Base Front',
-      'path': 'predefined_poses/base_front.yaml',
-      'joints': [0.0, 0.0125, 2.1765, 0.0, 0.7608, 0.0],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [2, 3],
-      'safe_previous_poses': [0, 3, 4, 5, 6]
-    },
-    {
-      'id': 3,
-      'name': 'Base Left',
-      'path': 'predefined_poses/base_left.yaml',
-      'joints': [2.2435, 0.3388, 1.7765, 0.0, 1.0902, 0.7],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [2, 3],
-      'safe_previous_poses': [0, 2, 4, 5]
-    },
-    {
-      'id': 4,
-      'name': 'Base Right',
-      'path': 'predefined_poses/base_right.yaml',
-      'joints': [-2.2435, 0.3388, 1.7765, 0.0, 1.0902, -0.7],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [2, 3],
-      'safe_previous_poses': [0, 2, 3, 5]
-    },
-    {
-      'id': 5,
       'name': 'Based',
       'path': 'predefined_poses/reset.yaml',
       'joints': [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
       'joints_set': [1, 2, 3, 4, 5, 6],
       'joints_checked': [],
       'safe_previous_poses': [0, 2, 3, 4, 5, 6]
-    },
-    {
-      'id': 6,
-      'name': 'Sand Storage',
-      'path': 'predefined_poses/sand_storage.yaml',
-      'joints': [-2.47, 0.08, 1.69, 0.6, 0.98, -0.88],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [],
-      'safe_previous_poses': [0, 2, 5]
-    },
-    {
-      'id': 7,
-      'name': 'Rock Storage',
-      'path': 'predefined_poses/rock_storage.yaml',
-      'joints': [-3.14, -0.7, 2.2, 0.0, 1.0, 0.0],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [],
-      'safe_previous_poses': [0, 2, 5]
-    },
-    {
-      'id': 8,
-      'name': 'Spectro Approach',
-      'path': 'predefined_poses/spectro_approach.yaml',
-      'joints': [0.0, 0.28, 2.4, 3.14, -1.7, 0.0],
-      'joints_set': [1, 2, 3, 4, 5, 6],
-      'joints_checked': [],
-      'safe_previous_poses': [0, 2, 5]
     }
   ]
 };
