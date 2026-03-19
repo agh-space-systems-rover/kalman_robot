b6736d1686cec29ded05f3cdb1cd36234295307d
Rafał Żelazko <rafal@zelazko.xyz>
2026-03-19 18:13:11 +0100

    fix missing DEV_URL; hide dev tools

--- kalman_gs/node_project/electron/main.js
+++ kalman_gs/node_project/electron/main.js
@@ -11,7 +11,7 @@ function createWindow() {
     webPreferences: {}
   });
 
-  window.loadURL(DEV_URL).then((response) => {
+  window.loadURL('http://localhost:3000').then((response) => {
     // nothing to do
   });
 
@@ -23,7 +23,7 @@ function createWindow() {
     window.focus();
   });
 
-  window.webContents.openDevTools({ mode: 'detach' });
+  // window.webContents.openDevTools({ mode: 'detach' });
 }
 
 app.whenReady().then(() => {
