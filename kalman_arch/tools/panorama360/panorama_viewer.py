f6341531c6f5d04ed85857c5a1db2de62add9f3f
Kacper Mazurek <mazurek04kacper@gmail.com>
2026-05-26 20:15:14 +0200

    panorama viewer

---
+++ kalman_arch/tools/panorama360/panorama_viewer.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import webbrowser
+from threading import Timer
+
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+IMAGE_PATH = ""
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/image")
+def get_image():
+    return send_file(IMAGE_PATH)
+
+
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Użycie: python panorama_viewer.py <path_to_panorama.jpg>")
+        sys.exit(1)
+
+    IMAGE_PATH = os.path.abspath(sys.argv[1])
+
+    if not os.path.exists(IMAGE_PATH):
+        print(f"Error: File '{IMAGE_PATH}' not exists.")
+        sys.exit(1)
+
+    print(f"Opening: {IMAGE_PATH}")
+    print("Opening browser...")
+
+    Timer(1, open_browser).start()
+
+    app.run(host="127.0.0.1", port=5000)
