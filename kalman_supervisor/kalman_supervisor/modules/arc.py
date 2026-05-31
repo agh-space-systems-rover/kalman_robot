a666fd921ebfe52e4bc460d229b3c203286de779
Kacper Mazurek <mazurek04kacper@gmail.com>
2026-05-31 23:48:55 +0200

    feat: supervisor arc.py

---
+++ kalman_supervisor/kalman_supervisor/modules/arc.py
@@ -0,0 +1,22 @@
+from kalman_supervisor.module import Module
+from std_msgs.msg import Float32
+
+class Arc(Module):
+    def __init__(self):
+        super().__init__("arch")
+        self.distance_sub = None
+
+    def activate(self) -> None:
+        self.distance_sub = self.supervisor.create_subscription(
+            Float32,
+            "/arc/distance_traveled",
+            self._distance_callback,
+            10
+        )
+
+    def deactivate(self) -> None:
+        if self.distance_sub:
+            self.supervisor.destroy_subscription(self.distance_sub)
+            
+    def get_distance_traveled(self) -> float:
+        return self._distance_traveled
