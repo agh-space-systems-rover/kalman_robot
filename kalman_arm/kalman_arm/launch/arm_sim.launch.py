1ee0339916a9b7a570843cb2402f8d71b891f393
Kacper Mazurek <mazurek04kacper@gmail.com>
2026-08-24 23:36:51 +0200

    added rviz vizualization

--- kalman_arm/kalman_arm/launch/arm_sim.launch.py
+++ kalman_arm/kalman_arm/launch/arm_sim.launch.py
@@ -57,6 +57,24 @@ def generate_launch_description():
         )
     )
 
+    description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("kalman_description"), "launch"),
+                "/description.launch.py",
+            ]
+        ),
+        launch_arguments={"layout": "arm"}.items()
+    )
+    rviz = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("kalman_rviz"), "launch"),
+                "/rviz.launch.py",
+            ]
+        ),
+        launch_arguments={"configs": "arm3"}.items()
+    )
     return LaunchDescription(
-        [arm_controller, master, servo, move_group, trajectories]
+        [arm_controller, master, servo, move_group, trajectories, description, rviz]
     )
