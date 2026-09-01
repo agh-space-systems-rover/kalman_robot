5339bf04edd21ef5e8dbf40866a484af2285e5a8
stanislawledzinski <20stachu@gmail.com>
2026-09-01 23:36:28 +0100

    refactor(kalman_arm): remove orphaned imu_filter_madgwick container

--- kalman_arm/kalman_arm/launch/drivers.launch.py
+++ kalman_arm/kalman_arm/launch/drivers.launch.py
@@ -1,9 +1,5 @@
-import os
-
 from launch import LaunchDescription
-
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
 from ament_index_python import get_package_share_path
 
 
@@ -29,52 +25,8 @@ def generate_launch_description():
         respawn_delay=30,
     )
 
-    phidgets_spatial_calibration_params_path = os.path.abspath(
-        os.path.join(
-            os.path.expanduser("~"),
-            ".config/kalman/phidgets_spatial_calibration_params.yaml",
-        )
-    )
-
-    imu_container = ComposableNodeContainer(
-        name="imu_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        respawn=True,
-        composable_node_descriptions=[
-            ComposableNode(
-                package="phidgets_spatial",
-                plugin="phidgets::SpatialRosI",
-                parameters=[
-                    str(
-                        get_package_share_path("kalman_arm")
-                        / "config"
-                        / "phidgets_spatial.yaml"
-                    ),
-                    phidgets_spatial_calibration_params_path,
-                ],
-                # NOTE: Spatial does not support intra-process communication.
-            ),
-            ComposableNode(
-                package="imu_filter_madgwick",
-                plugin="ImuFilterMadgwickRos",
-                parameters=[
-                    str(
-                        get_package_share_path("kalman_hardware")
-                        / "config"
-                        / "imu_filter_madgwick.yaml"
-                    ),
-                ],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            ),
-        ],
-        output="both",
-    )
-
     return LaunchDescription(
         [
             gps_node,
-            imu_container,
         ]
     )
